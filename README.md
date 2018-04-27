12e8bef416923a810308de2e21d53f178a009461
zyh19941109 <1124596031@qq.com>
2018-04-27 20:42:33 +0800

    hui

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
-# uu-shop
+## mpvue 踩坑之旅
 
-> A Mpvue project
+### 定位
 
-## Build Setup
+		引入腾讯的微信小程序JavaScript SDK
+		
+		因为微信小程序wx.getLocation API 返回的是地理位置坐标
+		
+		所以要用到地址逆解析，然后就是一顿复制
+		
+		var QQMapWX = require('xxx/qqmap-wx.js')...
+		
+		然后就出问题了，貌似SDK最后的代码是这样导出的module.exports = QQMapWX;
+		
+		改为export default QQMapWX; 引入改为import QQMapWX from 'XXX/qqmap-wx-jssdk.js'; 即可
 
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+[微信小程序JavaScript SDK](http://lbs.qq.com/qqmap_wx_jssdk/index.html)
--- src/pages/address/index.vue
+++ src/pages/address/index.vue
@@ -6,20 +6,47 @@
 
 <script>
     import util from '../../utils/index';
-    console.log(util)
+    // 引入SDK核心类
+    import QQMapWX from '../../utils/qqmap-wx-jssdk.js';
     export default {
         data() {
-            return {}
+            return {
+                latitude: null,
+                longitude: null
+            }
         },
         created() {},
         mounted() {
             wx.getLocation({
                 type: 'wgs84',
                 success: res => {
-                    console.log(res)
+                    //纬度
+                    this.latitude = res.latitude;
+                    //经度
+                    this.longitude = res.longitude;
                 },
                 fail: err => {}
             })
+            // 实例化API核心类
+            let address = new QQMapWX({
+                key: 'BZMBZ-OKXRU-DINVZ-2SRN5-4KWJ7-S6B6O'
+            })
+            // 调用接口
+            address.reverseGeocoder({
+                location: {
+                    latitude: this.latitude,
+                    longitude: this.longitude
+                },
+                success: function(res) {
+                    console.log(res);
+                },
+                fail: function(res) {
+                    console.log(res);
+                },
+                complete: function(res) {
+                    console.log(res);
+                }
+            });
         },
         components: {}
     }
