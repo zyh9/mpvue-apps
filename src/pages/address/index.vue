7d4ca1393f850fa1f22c428a379d5a365b323d16
zyh19941109 <1124596031@qq.com>
2018-04-27 21:04:32 +0800

    hui

--- src/pages/address/index.vue
+++ src/pages/address/index.vue
@@ -1,52 +1,52 @@
 <template>
-    <div>
-        我的收货地址
+    <div class="address_info">
+        正在定位：{{city}}
     </div>
 </template>
 
 <script>
     import util from '../../utils/index';
+    import msg from '../../utils/toast';
     // 引入SDK核心类
     import QQMapWX from '../../utils/qqmap-wx-jssdk.js';
+    const address = new QQMapWX({
+        key: 'BZMBZ-OKXRU-DINVZ-2SRN5-4KWJ7-S6B6O'
+    })
     export default {
         data() {
             return {
-                latitude: null,
-                longitude: null
+                city:null
             }
         },
         created() {},
         mounted() {
+            // 实例化API核心类
             wx.getLocation({
                 type: 'wgs84',
+                altitude:true,
                 success: res => {
-                    //纬度
-                    this.latitude = res.latitude;
-                    //经度
-                    this.longitude = res.longitude;
+                    this.map(res)
                 },
                 fail: err => {}
             })
-            // 实例化API核心类
-            let address = new QQMapWX({
-                key: 'BZMBZ-OKXRU-DINVZ-2SRN5-4KWJ7-S6B6O'
-            })
-            // 调用接口
-            address.reverseGeocoder({
-                location: {
-                    latitude: this.latitude,
-                    longitude: this.longitude
-                },
-                success: function(res) {
-                    console.log(res);
-                },
-                fail: function(res) {
-                    console.log(res);
-                },
-                complete: function(res) {
-                    console.log(res);
-                }
-            });
+        },
+        methods: {
+            map(info) {
+                // 调用接口
+                address.reverseGeocoder({
+                    location: {
+                        latitude: info.latitude,
+                        longitude: info.longitude
+                    },
+                    success: res => {
+                        console.log(res)
+                        this.city = res.result.address_component.city
+                    },
+                    fail: err => {
+                        msg('位置信息获取失败')
+                    }
+                })
+            }
         },
         components: {}
     }
