f12410e88cdbc9e6c24828663b2ac7261f75bb05
zyh9 <1124596031@qq.com>
2019-01-09 18:34:15 +0800

    demo同步

---
+++ Single-package/src/components/share.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="share_mask" v-if="shareBlock"></div>
+        <div class="share_card" :class="{share_card_active:shareBlock}">
+            <div class="share_card_btn">
+                <div class="btn_item">
+                    <i class="icon_share_wx btn_img"></i>
+                    <p>发给好友</p>
+                    <button class="btn_send" plain="true" open-type="share" @click="sendFriend"></button>
+                </div>
+                <div class="btn_item" @click="shareQR">
+                    <i class="icon_share_qr btn_img"></i>
+                    <p>二维码分享</p>
+                </div>
+            </div>
+            <div class="cancel" @click="cancelShare">取消</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                shareBlock: false
+            }
+        },
+        onLoad() {},
+        onReady() {},
+        onShow() {},
+        methods: {
+            blockShare() {
+                this.shareBlock = true;
+            },
+            hideShare() {
+                this.shareBlock = false;
+            },
+            sendFriend() {
+                this.$emit("sendFriend", {
+                    title: "发送好友"
+                })
+            },
+            shareQR() {
+                this.$emit("sendFriend", {
+                    title: "生成图片"
+                })
+            },
+            cancelShare() {
+                this.$emit("cancelShare", {
+                    title: "取消"
+                })
+            }
+        },
+        computed: {},
+        watch: {},
+        components: {}
+    }
+</script>
+
+<style lang="less">
+    @import url('../less/share.less');
+</style>
